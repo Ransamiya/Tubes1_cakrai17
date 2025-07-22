0df78c2acfc5994644b52e25628e361167ab11ef
Ransamiya <96335339+Ransamiya@users.noreply.github.com>
2025-07-22 23:42:20 +0700

    Update FSM.cpp

--- FSM.cpp
+++ FSM.cpp
@@ -1,11 +1,16 @@
-#include "FSM.hpp"
+#include "fsm.hpp"
 #include <iostream>
 #include <string>
 
 using namespace std;
+
+FSM::~FSM() {
+    // Destructor kosong
+}
+
 FSM::FSM(uint32_t delay){
   moveCount=delay; //pake properti yang tidka digunakan
 }
 void FSM::start(){
-  cout << moveCount << "kalo ditambah 10, jadi " << moveCount + 10 << "\n";
+  cout << moveCount << " kalo ditambah 10, jadi " << moveCount + 10 << "\n";
 }
